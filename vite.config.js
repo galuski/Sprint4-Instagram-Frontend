395a287ea529f219b212c7854c1a7cc2310da409
Gal Luski <luskigal@gmail.com>
2024-02-20 13:22:12 +0200

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: '../backend/public',
-    emptyOutDir: true
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1600
   }
 })
