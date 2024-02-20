0cc80f119aecaec0d95dd1ad7a15e64ea5de7a56
Gal Luski <luskigal@gmail.com>
2024-02-20 14:08:36 +0200

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
+  build: {
   //   outDir: '../backend/public',
   //   emptyOutDir: true,
   //   chunkSizeWarningLimit: 1600
-  // }
+  }
 })
