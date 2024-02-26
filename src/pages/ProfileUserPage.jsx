834e4c18f64fbfe8ddbcc6c3866dc765d1c30bff
Gal Luski <luskigal@gmail.com>
2024-02-26 14:13:08 +0200

    profile user page

--- src/pages/ProfileUserPage.jsx
+++ src/pages/ProfileUserPage.jsx
@@ -1,14 +1,20 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch  } from 'react-redux'
 import { ProfileUserHeader } from '../cmps/ProfileUserHeader'
 import { ProfileUserFooter } from '../cmps/ProfileUserFooter'
+import { getUserById } from '../redux/actions/userActions'
 import { Outlet, useNavigate, useParams } from 'react-router-dom'
 import { useEffect } from 'react'
 import { Sidebar } from '../cmps/Sidebar'
 import Logo from "../cmps/Logo"
 
 export function ProfileUserPage() {
-    const user = useSelector(storeState => storeState.userModule.user)
-    const { userId } = useParams()
+    const user = useSelector(storeState => storeState.userModule.user);
+    const dispatch = useDispatch();
+    const { userId } = useParams();
+
+    useEffect(() => {
+        dispatch(getUserById(userId)); // Dispatch an action to get user by ID
+    }, [dispatch, userId]);
 
     return (
         <section className='profile-user-page'>
