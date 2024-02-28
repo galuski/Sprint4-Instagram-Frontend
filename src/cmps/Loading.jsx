86d805c22c425d139d2c5f422ba8777dc956c1cf
Gal Luski <luskigal@gmail.com>
2024-02-28 17:26:20 +0200

    photos loading

--- src/cmps/Loading.jsx
+++ src/cmps/Loading.jsx
@@ -1,8 +1,8 @@
 export function Loading() {
     return (
         <section className="loading-page">
-            <img className="loading-instagram" src="./instagram-logo.png"></img>
-            <img className="loading-meta" src="./from-meta.png"></img>
+            <img className="loading-instagram" src="https://res.cloudinary.com/dkyjustoe/image/upload/v1709133903/instagram-logo_gxzjjk.png"></img>
+            <img className="loading-meta" src="https://res.cloudinary.com/dkyjustoe/image/upload/v1709133903/from-meta_wrkep4.png"></img>
         </section>
     )
 }
