e04a7b859c19260888fbed9c57e0903ec3ad436c
Gal Luski <luskigal@gmail.com>
2024-02-28 23:00:31 +0200

    loading page

--- src/cmps/Loading.jsx
+++ src/cmps/Loading.jsx
@@ -1,8 +1,12 @@
 export function Loading() {
     return (
         <section className="loading-page">
-            <img className="loading-instagram" src="https://res.cloudinary.com/dkyjustoe/image/upload/v1709133903/instagram-logo_gxzjjk.png"></img>
-            <img className="loading-meta" src="https://res.cloudinary.com/dkyjustoe/image/upload/v1709133903/from-meta_wrkep4.png"></img>
+            <div className="loading-instagram">
+                <img src="https://res.cloudinary.com/dkyjustoe/image/upload/v1709133903/instagram-logo_gxzjjk.png" alt="Instagram logo" />
+            </div>
+            <div className="loading-meta">
+                <img src="https://res.cloudinary.com/dkyjustoe/image/upload/v1709133903/from-meta_wrkep4.png" alt="Meta logo" />
+            </div>
         </section>
-    )
+    );
 }
