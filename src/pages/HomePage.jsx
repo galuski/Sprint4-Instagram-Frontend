65cd61de96d286c351380cb4962f0caace965609
Gal Luski <luskigal@gmail.com>
2024-03-01 16:36:24 +0200

    F/ home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,8 @@
-
 import { Sidebar } from "../cmps/Sidebar"
 import { PostList } from "../cmps/PostList"
 import { SuggestFollowers } from "../cmps/SuggestFollowers"
 import React, { useEffect, useRef, useState } from "react"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux" // Added useDispatch
 import { CreatePostModal } from "../cmps/CreatePostModal"
 import { Loading } from '../cmps/Loading'
 import { addPost, loadPosts, removePost } from "../store/post.actions"
@@ -14,6 +13,7 @@ import Logo from "../cmps/Logo"
 
 
 export function HomePage() {
+    const dispatch = useDispatch(); // Added useDispatch
     const posts = useSelector(storeState => storeState.postModule.posts)
     const users = useSelector(storeState => storeState.userModule.users)
     const [isLoading, setIsLoading] = useState(true)
@@ -23,6 +23,10 @@ export function HomePage() {
         loadPosts()
         loadUsers()
     }, [])
+
+    useEffect(() => { // Added useEffect for refreshing the page
+        loadPage();
+    }, []);
 
     const loadPage = () => {
         setTimeout(() => {
@@ -38,10 +42,10 @@ export function HomePage() {
 
     async function onAddPost(txt, file) {
         try {
-        const newPost = postService.getEmptyPost()
-        newPost.txt=txt
-        newPost.imgUrl=file
-            addPost(newPost)
+            const newPost = postService.getEmptyPost()
+            newPost.txt = txt
+            newPost.imgUrl = file
+            dispatch(addPost(newPost)); // Dispatch addPost action
         } catch (error) {
             console.log(error)
         } finally {
@@ -54,9 +58,9 @@ export function HomePage() {
         <>
             {openCreate ? <div className="create-modal"><CreatePostModal onAddPost={onAddPost} onCloseModal={ToggleModal} /></div> : null}
             <section className={`home-page-container `} >
-            <div className="logo-mobile">
-                <Logo />
-            </div>
+                <div className="logo-mobile">
+                    <Logo />
+                </div>
                 <section className="side-bar">
                     <Sidebar onCreate={setOpenCreate} />
                 </section>
